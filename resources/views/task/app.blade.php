d7ae70de9aefafd246c2e6132d0a1675c2c62528
Arlan Tri Handika <handikaarlan@gmail.com>
2024-07-07 00:33:45 +0700

    minor fix

--- resources/views/task/app.blade.php
+++ resources/views/task/app.blade.php
@@ -5,14 +5,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Aplikasi To Do List dengan Laravel 11">
-    <meta name="author" content="handikatriarlan">
 
     <!-- Title -->
     <title>To-Do-List</title>
-
-    <!-- Favicon icon -->
-    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
